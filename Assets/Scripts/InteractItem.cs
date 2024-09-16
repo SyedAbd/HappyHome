23a4316c3e081b94e2df9778564bd9c954253d17
Shah <syedabdullahamjad@gmail.com>
2024-09-16 09:06:38 +0300

    pickup and drop system added

--- Assets/Scripts/InteractItem.cs
+++ Assets/Scripts/InteractItem.cs
@@ -1,22 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro; // Add this to use TextMeshPro
+using TMPro;
 
 public class InteractItem : MonoBehaviour
 {
-    [SerializeField] GameObject player; // Reference to the player
-    [SerializeField] TextMeshProUGUI interactionText; // Reference to TMP for UI text
+    [SerializeField] GameObject player; 
+    [SerializeField] TextMeshProUGUI interactionText; 
+    [SerializeField] GameObject keyPrefab; 
     private bool isPickedUp;
-    private bool isInRange; // To check if the player is in range to pick up the item
+    private bool isInRange;
     private Vector2 speed;
     public float smoothTime;
-
+    private Vector3 initialPosition;
+    private Vector3 offset = new Vector3(0, 1.5f, 0);
     void Start()
     {
         isPickedUp = false;
         isInRange = false;
-        interactionText.enabled = false; // Hide the message initially
+        interactionText.enabled = false; 
+        initialPosition = transform.position; 
     }
 
     void Update()
@@ -24,27 +27,43 @@ public class InteractItem : MonoBehaviour
         if (isInRange && !isPickedUp)
         {
             interactionText.text = "Press E to pick up the key";
-            interactionText.enabled = true; // Show the message when the player is in range
+            interactionText.enabled = true;
 
             if (Input.GetKeyDown(KeyCode.E))
             {
                 isPickedUp = true;
-                interactionText.enabled = false; // Hide the message once the key is picked up
+                interactionText.enabled = false; 
             }
         }
 
         if (isPickedUp)
         {
-            Vector3 offset = new Vector3(0, 1.5f, 0);
+            
+            
             transform.position = Vector2.SmoothDamp(transform.position, player.transform.position + offset, ref speed, smoothTime);
+
+            
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                DropItem(); 
+            }
         }
+    }
+
+    private void DropItem()
+    {
+        
+        Instantiate(keyPrefab, transform.position - offset, Quaternion.identity);
+
+        
+        Destroy(gameObject);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player") && !isPickedUp)
         {
-            isInRange = true; // Player is in range
+            isInRange = true; 
         }
     }
 
@@ -52,8 +71,8 @@ public class InteractItem : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player") && !isPickedUp)
         {
-            isInRange = false; // Player is out of range
-            interactionText.enabled = false; // Hide the message when the player leaves
+            isInRange = false; 
+            interactionText.enabled = false; 
         }
     }
 }
