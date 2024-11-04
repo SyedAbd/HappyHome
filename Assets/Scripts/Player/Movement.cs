cc1686762960312ecb0cd670ac5068c3080ed0db
Shah <syedabdullahamjad@gmail.com>
2024-11-04 09:30:01 +0200

    initial animation

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -5,13 +5,24 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
-
     private float movement;
+    private Animator animator;
+
+    void Start()
+    {
+        
+        animator = GetComponent<Animator>();
+    }
 
     void Update()
     {
         movement = Input.GetAxis("Horizontal");
 
+        
         transform.Translate(Vector3.right * movement * moveSpeed * Time.deltaTime);
+
+        
+        animator.SetBool("IsWalking", movement != 0);
+
     }
 }
